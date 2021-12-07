6e192b96e150889851b01469fb360c14fc1a38ed
hadoyun <55866690+hadoyun@users.noreply.github.com>
2021-12-08 01:37:11 +0900

    ctx 사용!
    get context (2d)
    set ctx size of width and height!

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
 const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
+
+//ctx는 2가지 사이즈를 줘야한다.
+canvas.width = 700;
+canvas.height = 700;
 
 let painting = false;
 
-function stopPainging(){
 
-}
 
-function onMouseMove(event){
-    const x = event.offsetX;
-    const y = event.offsetY;
+//////////////////////////////////////////////////////////////
 
-}
-
-function onMouseDown(event){
+function startPainting() {
     painting = true;
 }
 
-function onMouseUp(event){
-    stopPainging();
+function stopPainting() {
+    painting = false;
 }
 
-if(canvas){
+//움직임 감지
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event) {
+    painting = true;
+}
+
+if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave", onMouseLeave);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
